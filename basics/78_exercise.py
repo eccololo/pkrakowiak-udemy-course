1816d2cfb6f5f56566755b79f9fe6064f5bfb848
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-27 13:15:54 +0200

    Solve exercise 78.

---
+++ basics/78_exercise.py
@@ -0,0 +1,28 @@
+# Wykorzystując odpowiednią funkcję sprawdź typ podanych obiektów:
+#
+# (1, 2, 3)
+#
+# {1, 2, 3}
+#
+# [1, 2, 3]
+#
+# {1: 1, 2: 2, 3: 3}
+#
+# '123'
+#
+# 123
+#
+# W odpowiedzi wydrukuj każdy typ do konsoli w osobnej linii tak jak pokazano poniżej.
+#
+
+print(type((1, 2, 3)))
+
+print(type({1, 2, 3}))
+
+print(type([1, 2, 3]))
+
+print(type({1: 1, 2: 2, 3: 3}))
+
+print(type('123'))
+
+print(type(123))
