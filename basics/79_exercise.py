39cb059ae5f5312861a648512a742150785c80e3
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-27 13:18:03 +0200

    Solve exercise 79.

---
+++ basics/79_exercise.py
@@ -0,0 +1,24 @@
+# Podana jest poniższa lista:
+#
+#
+#
+# numbers = [5, 2, 8, 1, 9, 3]
+#
+#
+# Wykorzystując odpowiednie funkcje wbudowane wyznacz wartość największą, wartość najmniejszą i długość podanej listy. Wynik wydrukuj do konsoli tak jak pokazano poniżej.
+#
+#
+#
+# Oczekiwany wynik:
+#
+#
+#
+# Maximum: 9
+# Minimum: 1
+# Length: 6
+
+numbers = [5, 2, 8, 1, 9, 3]
+
+print(f"Maximum: {max(numbers)}")
+print(f"Minimum: {min(numbers)}")
+print(f"Length: {len(numbers)}")
