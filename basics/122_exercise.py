5f22b193839675b3a572a85b23e09fde9cc4d930
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-08 12:52:57 +0200

    Exercise 122 done.

---
+++ basics/122_exercise.py
@@ -0,0 +1,35 @@
+# Zdefiniuj klasę Product z trzema atrybutami klasy:
+#
+# category
+#
+# brand
+#
+# price
+#
+#
+#
+# Ustaw wartości tych atrybutów odpowiednio na:
+#
+# 'Electronics'
+#
+# 'Samsung'
+#
+# 500.0
+#
+#
+#
+# Te atrybuty będą wspólne dla wszystkich instancji klasy i są dostępne za pomocą nazwy klasy.
+#
+#
+#
+# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli.
+
+class Product:
+    category = 'Electronics'
+    brand = 'Samsung'
+    price = 500.0
+
+
+
+
+
