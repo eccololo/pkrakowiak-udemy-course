1d721c332f1c127e3e11d5a3bac5a6105e3b983f
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-08 12:06:41 +0200

    Solve exercise 113.

---
+++ basics/113_exercise.py
@@ -0,0 +1,31 @@
+# Podany jest moduł o nazwie math_functions.py, który zawiera funkcje związane z prostymi działaniami matematycznymi. Funkcji tych chcesz użyć w innym pliku o nazwie exercise.py.
+#
+#
+#
+# Zaimportuj odpowiednio moduł math_functions i wykonaj poniższe operacje:
+#
+# square(10)
+#
+# cube(3)
+#
+# add(10, 5)
+#
+#
+#
+# Wyniki otrzymanych operacji wydrukuj do konsoli tak jak pokazano poniżej.
+#
+#
+#
+# Oczekiwany wynik:
+#
+#
+#
+# 100
+# 27
+# 15
+
+from math_functions_113 import *
+print(square(10))
+print(cube(3))
+print(add(10, 5))
+
---
+++ basics/math_functions_113.py
@@ -0,0 +1,11 @@
+def square(x):
+    """Returns the square of a number."""
+    return x ** 2
+
+def cube(x):
+    """Returns the cube of a number."""
+    return x ** 3
+
+def add(x, y):
+    """Returns the sum of two numbers."""
+    return x + y
