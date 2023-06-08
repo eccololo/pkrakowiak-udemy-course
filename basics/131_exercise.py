7e5a5fce70ac6867136c0f8fa2462617c1bc09d4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-08 13:34:15 +0200

    Exercise 131 done.

---
+++ basics/131_exercise.py
@@ -0,0 +1,113 @@
+# Załóżmy, że
+# przeprowadzasz
+# eksperyment
+# mający
+# na
+# celu
+# zmierzenie
+# temperatury
+# substancji
+# w
+# czasie.Chcesz
+# utworzyć
+# klasę
+# do
+# reprezentowania
+# substancji
+# i
+# śledzenia
+# jej
+# temperatury
+# w
+# czasie.Do
+# tej
+# pory
+# masz
+# poniższą
+# implementację:
+#
+#
+# class Substance:
+#     all_substances = []
+#
+#     def __init__(self, name, initial_temp):
+#         self.name = name
+#         self.temperature = [initial_temp]
+#         Substance.all_substances.append(self)
+#
+#     def add_temperature(self, temp):
+#         self.temperature.append(temp)
+#
+#
+# Zaimplementuj
+# również
+# metodę
+# klasy
+# o
+# nazwie
+# average_temperature(), która
+# oblicza
+# średnią
+# temperaturę
+# wszystkich
+# utworzonych
+# substancji.
+#
+# Przykład
+# użycia
+# klasy
+# Substance:
+#
+# [IN]: water = Substance('Water', 20)
+# [IN]: water.add_temperature(21)
+# [IN]: water.add_temperature(22)
+#
+# [IN]: oil = Substance('Oil', 25)
+# [IN]: oil.add_temperature(26)
+# [IN]: oil.add_temperature(27)
+#
+# [IN]: Substance.average_temperature()
+#
+# [OUT]: 23.5
+#
+# Uwaga! W
+# celu
+# zaliczenia
+# ćwiczenia
+# należy
+# tylko
+# zaimplementować
+# klasę.Nie
+# trzeba
+# niczego
+# drukować
+# do
+# konsoli.Testy
+# jednostkowe
+# sprawdzają
+# poprawność
+# implementacji
+# rozwiązania.
+
+class Substance:
+    all_substances = []
+
+    def __init__(self, name, initial_temp):
+        self.name = name
+        self.temperature = [initial_temp]
+        Substance.all_substances.append(self)
+
+    def add_temperature(self, temp):
+        self.temperature.append(temp)
+
+    # Enter your solution here
+    @classmethod
+    def average_temperature(cls):
+        total_temp = 0
+        len_temp = 0
+        for subst in cls.all_substances:
+            total_temp += sum(subst.temperature)
+            len_temp += len(subst.temperature)
+
+        return total_temp / len_temp
+
