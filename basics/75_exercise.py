fac2e03de56679b90f8ddf9871bf0a4dca401399
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:53:42 +0200

    Change exercise desc in exercise 75.

--- basics/75_exercise.py
+++ basics/75_exercise.py
@@ -1,53 +1,13 @@
-# Podana
-# jest
-# poniższa
-# lista
-# produktów:
-#
-# products = [
+# [IN]: [
 #     {'name': 'T-shirt', 'price': 29.99},
 #     {'name': 'Shoes', 'price': 99.99},
 #     {'name': 'Pants', 'price': 49.99},
 # ]
-#
-# Zapisz
-# listę
-# produktów
-# do
-# pliku
-# tekstowego
-# o
-# nazwie
-# products.txt.Każdy
-# produkt
-# zapisz
-# jako
-# wiersz, który
-# składa
-# się
-# z
-# nazwy
-# produktu
-# i
-# ceny, oddzielonych
-# przecinkiem.Każdy
-# wiersz
-# zakończ
-# znakiem
-# nowej
-# linii \n.
-#
-# Zawartość
-# pliku
-# products.txt
-# po
-# zapisaniu
-# danych:
-#
-# T - shirt, 29.99
+
+# [OUT]: 75_products.txt file:
+# T-shirt, 29.99
 # Shoes, 99.99
 # Pants, 49.99
-#
 
 products = [
     {'name': 'T-shirt', 'price': 29.99},
