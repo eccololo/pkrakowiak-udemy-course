857872554d98f02356350ed51b62619535bcb505
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:46:32 +0200

    Change exercise desc in exercise 124.

--- basics/124_exercise.py
+++ basics/124_exercise.py
@@ -1,23 +1,5 @@
-# Załóżmy, że pracujesz nad projektem e-commerce. Masz już utworzoną klasę o nazwie Product reprezentującą produkt w asortymencie sklepu internetowego:
-#
-#
-#
-# class Product:
-#     def __init__(self, name, price, quantity):
-#         self.name = name
-#         self.price = price
-#         self.quantity = quantity
-#
-#
-# Zaimplementuj w klasie Product dwie metody umożliwiające użytkownikowi aktualizację ceny i ilości produktu odpowiednio o nazwach:
-#
-# update_price()
-#
-# update_quantity()
-#
-#
-#
-# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli. Testy jednostkowe sprawdzają poprawność implementacji rozwiązania.
+# Stworz klase o nazwie Product. W konstruktorze zainicjuj zmienne o nazwie name, price i quantity.
+# Utworz settery dla price i quantity.
 
 class Product:
     def __init__(self, name, price, quantity):
