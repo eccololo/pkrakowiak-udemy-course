31aac83a8bd18ad8ab5b2b204d19959956cbeb02
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:03:02 +0200

    Change exercise desc in exercise 83.

--- basics/83_exercise.py
+++ basics/83_exercise.py
@@ -1,17 +1,7 @@
-# Przy pomocy odpowiedniej funkcji wbudowanej oraz pętli for wydrukuj poniższe wartości do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [OUT]:
 # 1 2 3 4 5 6 7 8 9
 # 0 2 4 6 8
 # 100 90 80 70 60 50 40 30 20 10
-#
-#
-# Zwróć uwagę na białe znaki. Po każdej liczbie wstawiony został znak spacji.
 
 output_1 = [f"{number }" for number in range(1, 10)]
 output_2 = [f"{number }" for number in range(9) if number % 2 == 0]
