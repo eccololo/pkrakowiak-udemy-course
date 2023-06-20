87279e8725be8f5699fe6ab9c65e52f2523670fa
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:07:55 +0200

    Change exercise desc in exercise 90.

--- basics/90_exercise.py
+++ basics/90_exercise.py
@@ -1,19 +1,6 @@
-# Podana jest lista miast:
-#
-#
-#
-# cities = ['Warsaw', 'London', 'Berlin', 'New York']
-#
-#
-# Używając funkcji map() oraz wyrażenia lambda uzyskaj listę zawierającą trzy pierwsze litery każdego miasta. Otrzymaną listę wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# ['War', 'Lon', 'Ber', 'New']
+# [IN]: ['Warsaw', 'London', 'Berlin', 'New York']
+
+# [OUT]: ['War', 'Lon', 'Ber', 'New']
 
 cities = ['Warsaw', 'London', 'Berlin', 'New York']
 
