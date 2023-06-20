3047c5f5b535d1f1f38155619748cb524e8ecb79
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:06:01 +0200

    Change exercise desc in exercise 87.

--- basics/87_exercise.py
+++ basics/87_exercise.py
@@ -1,24 +1,8 @@
-# Zaimplementuj funkcję o nazwie rectangle_area(), która obliczy pole prostokąta.
-#
-#
-#
-# Przykłady użycia:
-#
-#
-#
 # [IN]: rectangle_area(2, 4)
 # [OUT]: 8
 #
-#
 # [IN]: rectangle_area(10, 6)
 # [OUT]: 60
-#
-#
-# W tym ćwiczeniu nie dokonujemy żadnej walidacji argumentów. Zakładamy, że użytkownik przekazuje dodatnie wartości liczbowe.
-#
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować funkcję. Nie trzeba jej wywoływać. Zaimplementowane testy sprawdzają poprawność działania funkcji.
 
 def rectangle_area(a, b):
     return a * b
