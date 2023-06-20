707482e880b49d5ed79c60cdaf4b78b08e591ea2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:10:45 +0200

    Change exercise desc in exercise 95.

--- basics/95_exercise.py
+++ basics/95_exercise.py
@@ -1,32 +1,14 @@
-# Zaimplementuj funkcję o nazwie count_kwargs(), która będzie zwracać liczbę przekazanych argumentów typu klucz-wartość (keyword arguments) do funkcji.
-#
-#
-#
-# Przykłady użycia:
-#
-#
-#
 # [IN]: count_kwargs(a=1, b=3)
 # [OUT]: 2
-#
 #
 # [IN]: count_kwargs(10, 'John', age=20, country='USA')
 # [OUT]: 2
 #
-#
 # [IN]: count_kwargs(30, 1)
 # [OUT]: 0
 #
-#
 # [IN]: count_kwargs()
 # [OUT]: 0
-#
-#
-# W tym ćwiczeniu nie dokonujemy żadnej walidacji argumentów.
-#
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować funkcję. Nie trzeba jej wywoływać. Zaimplementowane testy sprawdzają poprawność działania funkcji.
 
 def count_kwargs(**kwargs):
     return len(kwargs)
