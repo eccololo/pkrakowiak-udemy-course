56075720f3ad3096eba60cefc50ef3e83b6e76db
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:25:16 +0200

    Change exercise desc in exercise 106.

--- basics/106_exercise.py
+++ basics/106_exercise.py
@@ -1,24 +1,11 @@
-# Załóżmy, że masz listę krotek, z których każda zawiera nazwę języka programowania i odpowiadające mu rozszerzenie pliku:
-#
-#
-#
-# languages = [
+# [IN]: [
 #     ('Python', '.py'),
 #     ('JavaScript', '.js'),
 #     ('C++', '.cpp'),
 #     ('Java', '.java'),
 # ]
-#
-#
-# Potrzebujesz utworzyć nowy słownik, który odwzorowuje każde rozszerzenie pliku na odpowiadający mu język programowania. Otrzymany słownik wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# {'.py': 'Python', '.js': 'JavaScript', '.cpp': 'C++', '.java': 'Java'}
+
+# [OUT]: {'.py': 'Python', '.js': 'JavaScript', '.cpp': 'C++', '.java': 'Java'}
 
 
 languages = [
