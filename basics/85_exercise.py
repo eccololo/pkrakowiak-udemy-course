67869c97a5d03e82d8af2b27ecb77547c5312890
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:04:32 +0200

    Change exercise desc in exercise 85.

--- basics/85_exercise.py
+++ basics/85_exercise.py
@@ -1,12 +1,4 @@
-# Napisz funkcję calculate_average() która przyjmie trzy argumenty i zwróci ich średnią wartość.
-#
-#
-#
-# W tym ćwiczeniu nie dokonujemy żadnej walidacji argumentów. Zakładamy, że użytkownik przekazuje wartości liczbowe.
-#
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować funkcję. Nie trzeba jej wywoływać. Zaimplementowane testy sprawdzają poprawność działania funkcji.
+# Napisz funkcję calculate_average() obliczajaca srednia arytmetyczna
 
 def calculate_average(*args):
     return sum(args) / len(args)
