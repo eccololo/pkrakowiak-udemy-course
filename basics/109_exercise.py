89f9e481cda7588553fcafe7d5383b6fee6a007d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:27:21 +0200

    Change exercise desc in exercise 109.

--- basics/109_exercise.py
+++ basics/109_exercise.py
@@ -1,8 +1,4 @@
-# Załóżmy, że masz listę piłkarzy, gdzie każdy gracz jest reprezentowany przez słownik zawierający nazwisko, pozycję i bramki zdobyte w bieżącym sezonie:
-#
-#
-#
-# players = [
+# [IN]:  [
 #     {'name': 'Alice', 'position': 'Forward', 'goals': 15},
 #     {'name': 'Bob', 'position': 'Midfielder', 'goals': 7},
 #     {'name': 'Charlie', 'position': 'Defender', 'goals': 2},
@@ -10,17 +6,7 @@
 #     {'name': 'Eve', 'position': 'Forward', 'goals': 12},
 #     {'name': 'Frank', 'position': 'Midfielder', 'goals': 5},
 # ]
-#
-#
-# Potrzebujesz stworzyć nowy słownik, który odwzorowuje każdą pozycję na całkowitą liczbę bramek zdobytych przez graczy na tej pozycji. W odpowiedzi wydrukuj otrzymany słownik do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# {'Forward': 27, 'Midfielder': 12, 'Defender': 2, 'Goalkeeper': 0}
+# [OUT]: {'Forward': 27, 'Midfielder': 12, 'Defender': 2, 'Goalkeeper': 0}
 
 players = [
     {'name': 'Alice', 'position': 'Forward', 'goals': 15},
