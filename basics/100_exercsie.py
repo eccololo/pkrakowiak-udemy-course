e1790285530b1fb362ea154c0e3eb9a4ad1403ae
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:20:12 +0200

    Change exercise desc in exercise 100.

--- basics/100_exercsie.py
+++ basics/100_exercsie.py
@@ -1,11 +1,3 @@
-# Napisz generator o nazwie geometric_sequence(), który będzie zwracać kolejne liczby z ciągu geometrycznego o zadanym pierwszym wyrazie i ilorazie. Generator powinien działać w nieskończoność, tzn. zwracać kolejne wyrazy ciągu aż do przekroczenia zakresu typu int.
-#
-#
-#
-# Przykłady użycia:
-#
-#
-#
 # [IN]: geo = geometric_sequence(1, 2)
 # [IN]: next(geo)
 # [OUT]: 1
@@ -19,9 +11,6 @@
 # [OUT]: 16
 # [IN]: next(geo)
 # [OUT]: 32
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować generator. Nie trzeba go wywoływać. Zaimplementowane testy sprawdzają poprawność działania generatora.
 
 def geometric_sequence(wyraz, iloraz):
     n = 1
