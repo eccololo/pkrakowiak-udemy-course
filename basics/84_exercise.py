4524c1baff57ead0bcd748cd1e05bcd5d3143910
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:03:50 +0200

    Change exercise desc in exercise 84.

--- basics/84_exercise.py
+++ basics/84_exercise.py
@@ -1,19 +1,5 @@
-# Podana jest poniższa lista:
-#
-#
-#
-# values = ['5', '2', '8', '1', '9', '3']
-#
-#
-# Używając funkcji wbudowanej map() dokonaj konwersji elementów listy na typ int. Otrzymaną listę wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# [5, 2, 8, 1, 9, 3]
+# [IN]: ['5', '2', '8', '1', '9', '3']
+# [OUT]: [5, 2, 8, 1, 9, 3]
 
 values = ['5', '2', '8', '1', '9', '3']
 output = list(map(lambda x: int(x), values))
