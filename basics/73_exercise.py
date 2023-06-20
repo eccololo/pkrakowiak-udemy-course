68cfa5e9c441b2dc9590cb3fba5cdc0cfd60e253
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:50:44 +0200

    Change exercise desc in exercise 73.

--- basics/73_exercise.py
+++ basics/73_exercise.py
@@ -1,7 +1,5 @@
-# Napisz program, który wczytuje ten plik CSV, przetwarza te dane i
-# oblicza średnią energię wygenerowaną przez elektrownię wodną
-# (trzecia kolumna pliku, energia wyrażona w kWh).
-# Wynik wydrukuj do konsoli tak jak pokazano poniżej.
+# [IN]: 73_data.csv
+# [OUT]: Average energy generated: 155.73 kWh
 
 with open("73_data.csv", "r") as f:
     data = f.readlines()
