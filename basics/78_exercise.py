71bbbb5650d519b3954ebe7b7cb1ed2a059ced45
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:57:50 +0200

    Change exercise desc in exercise 78.

--- basics/78_exercise.py
+++ basics/78_exercise.py
@@ -1,19 +1,19 @@
-# Wykorzystując odpowiednią funkcję sprawdź typ podanych obiektów:
-#
+# [IN]:
 # (1, 2, 3)
-#
 # {1, 2, 3}
-#
 # [1, 2, 3]
-#
 # {1: 1, 2: 2, 3: 3}
-#
 # '123'
-#
 # 123
-#
-# W odpowiedzi wydrukuj każdy typ do konsoli w osobnej linii tak jak pokazano poniżej.
-#
+
+# [OUT]:
+# <class 'tuple'>
+# <class 'set'>
+# <class 'list'>
+# <class 'dict'>
+# <class 'str'>
+# <class 'int'>
+
 
 print(type((1, 2, 3)))
 
