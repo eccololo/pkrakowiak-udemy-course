21cd8843d4263ad028d9102c43925f947e0abd44
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:22:36 +0200

    Change exercise desc in exercise 103.

--- basics/103_exercise.py
+++ basics/103_exercise.py
@@ -1,19 +1,5 @@
-# Podana jest poniższa lista:
-#
-#
-#
-# words = ['cat', 'dog', 'rabbit', 'hamster', 'parrot', 'raccoon']
-#
-#
-# Wykorzystaj list comprehension, aby utworzyć nową listę o nazwie r_words, która zawiera tylko słowa z oryginalnej listy słów words, które zaczynają się na literę 'r'. W odpowiedzi wynik wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# # ['rabbit', 'raccoon'
+# [IN]: ['cat', 'dog', 'rabbit', 'hamster', 'parrot', 'raccoon']
+# [OUT]: ['rabbit', 'raccoon']
 
 words = ['cat', 'dog', 'rabbit', 'hamster', 'parrot', 'raccoon']
 
