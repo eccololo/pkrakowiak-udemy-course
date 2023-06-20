6368be31b7b4c1219cd1cfdb93c09e72ec01c249
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:24:13 +0200

    Change exercise desc in exercise 105.

--- basics/105_exercise.py
+++ basics/105_exercise.py
@@ -1,25 +1,12 @@
-# Załóżmy, że masz przeanalizować listę rekordów pacjentów, gdzie każdy rekord jest słownikiem zawierającym informacje o pacjencie, w tym jego imię i nazwisko, wiek oraz odczyty ciśnienia krwi (skurczowego - systolic i rozkurczowego - diastolic):
-#
-#
-#
-# patients = [
+# [IN]: [
 #     {'name': 'Alice', 'age': 45, 'systolic': 140, 'diastolic': 90},
 #     {'name': 'Bob', 'age': 38, 'systolic': 120, 'diastolic': 80},
 #     {'name': 'Charlie', 'age': 55, 'systolic': 150, 'diastolic': 95},
 #     {'name': 'David', 'age': 62, 'systolic': 130, 'diastolic': 100},
 #     {'name': 'Eve', 'age': 29, 'systolic': 135, 'diastolic': 85},
 # ]
-#
-#
-# Potrzebujesz utworzyć nową listę zawierającą tylko nazwiska pacjentów z wysokim ciśnieniem krwi, zdefiniowanym jako odczyt skurczowy większy niż 140 i/lub odczyt rozkurczowy większy niż 90. Otrzymaną listę wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# ['Charlie', 'David']
+
+# [OUT]: ['Charlie', 'David']
 
 patients = [
     {'name': 'Alice', 'age': 45, 'systolic': 140, 'diastolic': 90},
