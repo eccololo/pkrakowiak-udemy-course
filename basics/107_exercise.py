88af5a7e80d12ae47364e2e37cc0a43cdaf6f961
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:25:58 +0200

    Change exercise desc in exercise 107.

--- basics/107_exercise.py
+++ basics/107_exercise.py
@@ -1,8 +1,4 @@
-# Załóżmy, że masz listę żołnierzy w jednostce wojskowej, gdzie każdy żołnierz jest reprezentowany przez słownik zawierający nazwisko, stopień i lata służby:
-#
-#
-#
-# soldiers = [
+# [IN]:  [
 #     {'name': 'Alice', 'rank': 'Private', 'service_years': 2},
 #     {'name': 'Bob', 'rank': 'Sergeant', 'service_years': 4},
 #     {'name': 'Charlie', 'rank': 'Sergeant', 'service_years': 6},
@@ -10,17 +6,8 @@
 #     {'name': 'Eve', 'rank': 'Private', 'service_years': 1},
 #     {'name': 'Frank', 'rank': 'Lieutenant', 'service_years': 7},
 # ]
-#
-#
-# Chcesz stworzyć nowy słownik, który odwzorowuje każdą rangę na całkowitą liczbę żołnierzy w jednostce, którzy posiadają daną rangę (klucz rank). Otrzymaną listę wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# {'Private': 2, 'Sergeant': 2, 'Lieutenant': 2}
+
+# [OUT]: {'Private': 2, 'Sergeant': 2, 'Lieutenant': 2}
 
 soldiers = [
     {'name': 'Alice', 'rank': 'Private', 'service_years': 2},
