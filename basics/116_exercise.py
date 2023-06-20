87474376746ebb5f738a12685166afa304e41889
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:39:14 +0200

    Change exercise desc in exercise 116.

--- basics/116_exercise.py
+++ basics/116_exercise.py
@@ -1,14 +1,9 @@
-# Załóżmy, że budujesz narzędzie do szyfrowania i deszyfrowania wiadomości za pomocą prostego szyfru podstawieniowego i chcesz użyć modułu cipher.py do obsługi operacji szyfrowania i deszyfrowania.
-#
-#
-#
-# Zaimportuj odpowiednio funkcję decrypt() i używając klucza 'phqgiumeaylnofdxkrcvstzwb' (argument key funkcji encrpyt()) odszyfruj poniższą wiadomość:
-#
+# [IN]:
+# 'phqgiumeaylnofdxkrcvstzwb'
 # 'Mripv! Bds gag av!'
-#
-#
-#
-# Odszyfrowaną wiadomość wydrukuj do konsoli.
+
+# [OUT]:
+# Great! You did it!
 
 from cipher_115 import decrypt
 key = 'phqgiumeaylnofdxkrcvstzwb'
