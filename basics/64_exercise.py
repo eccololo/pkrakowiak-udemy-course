765246338295c3a222895646b399740dc5865f79
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:46:00 +0200

    Change exercise desc in exercise 64.

--- basics/64_exercise.py
+++ basics/64_exercise.py
@@ -1,5 +1,58 @@
-# Napisz program, który wyświetli tylko te pokoje, które są dostępne w
-# określonym przedziale czasowym - od 2023-05-11 do 2023-05-13 włącznie.
+# [IN]: rooms = [
+#     {
+#         'number': 101,
+#         'available_dates': [
+#             '2023-05-10',
+#             '2023-05-11',
+#             '2023-05-12',
+#         ],
+#     },
+#     {
+#         'number': 102,
+#         'available_dates': [
+#             '2023-05-10',
+#             '2023-05-11',
+#         ],
+#     },
+#     {
+#         'number': 103,
+#         'available_dates': [
+#             '2023-05-11',
+#             '2023-05-12',
+#             '2023-05-13',
+#         ],
+#     },
+#     {
+#         'number': 105,
+#         'available_dates': [
+#             '2023-05-10',
+#             '2023-05-11',
+#             '2023-05-12',
+#             '2023-05-13',
+#         ],
+#     },
+#     {
+#         'number': 107,
+#         'available_dates': [
+#             '2023-05-11',
+#             '2023-05-12',
+#         ],
+#     },
+#     {
+#         'number': 110,
+#         'available_dates': [
+#             '2023-05-10',
+#             '2023-05-11',
+#             '2023-05-12',
+#             '2023-05-13',
+#         ],
+#     },
+# ]
+
+# [OUT]:
+# Room number 103 is available.
+# Room number 105 is available.
+# Room number 110 is available.
 
 rooms = [
     {
