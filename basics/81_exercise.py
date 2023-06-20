58c48cfd0c754f33477917536444a34d09933e33
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:00:43 +0200

    Change exercise desc in exercise 81.

--- basics/81_exercise.py
+++ basics/81_exercise.py
@@ -1,30 +1,9 @@
-# Podane są poniższe zmienne:
-#
-#
-#
-# number = 5
-# string = 'Hello, world!'
-# values = [5, 2, 8, 1, 9, 3]
-#
-#
-# Wykonaj poszczególne kroki:
-#
-# sprawdź, czy zmienna number jest typu int - jeśli tak, wydrukuj do konsoli 'The number is of type int.'
-#
-# sprawdź, czy zmienna number jest typu float - jeśli tak, wydrukuj do konsoli 'The number is of type float.'
-#
-# sprawdź, czy zmienna string jest typu str - jeśli tak, wydrukuj do konsoli 'The string is of type str.'
-#
-# sprawdź, czy zmienna values jest typu tuple - jeśli tak, wydrukuj do konsoli 'The object is of type tuple.'
-#
-# sprawdź, czy zmienna values jest typu list - jeśli tak, wydrukuj do konsoli 'The object is of type list.'
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [IN]:
+# 5
+# 'Hello, world!'
+# [5, 2, 8, 1, 9, 3]
+
+# [OUT]:
 # The number is of type int.
 # The string is of type str.
 # The object is of type list.
