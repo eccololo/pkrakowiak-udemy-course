e8851e453bd07566e6ee5a9e4d8f48c4f90b2ea1
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:45:11 +0200

    Change exercise desc in exercise 123.

--- basics/123_exercise.py
+++ basics/123_exercise.py
@@ -1,18 +1,4 @@
-# Załóżmy, że pracujesz nad projektem e-commerce i musisz utworzyć klasę o nazwie Product reprezentującą produkt w asortymencie sklepu internetowego. Klasa powinna mieć następujące atrybuty (atrybuty instancji):
-#
-# name: ciąg reprezentujący nazwę produktu
-#
-# price: liczba zmiennoprzecinkowa reprezentująca cenę produktu
-#
-# quantity: liczba całkowita reprezentująca ilość produktu w magazynie
-#
-#
-#
-# Wykorzystaj w implementacji metodę specjalną __init__() do ustawienia wymaganych atrybutów.
-#
-#
-#
-# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli. Testy jednostkowe sprawdzają poprawność implementacji rozwiązania.
+# Stworz klase o nazwie Product i w konstruktorze zainicjuj zmienne name, price i quantity.
 
 class Product:
     def __init__(self, name, price, quantity):
