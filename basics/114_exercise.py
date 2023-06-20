f9b902035e2e0f7a3b8cd4abb1582afd6433c5db
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:37:08 +0200

    Change exercise desc in exercise 114.

--- basics/114_exercise.py
+++ basics/114_exercise.py
@@ -1,25 +1,7 @@
-# Podany jest moduł o nazwie string_operations.py, który zawiera funkcje związane z prostymi operacjami na obiektach tekstowych. Z tego modułu potrzebujesz użyć funkcji count_vowels().
-#
-#
-#
-# Zaimportuj odpowiednio funkcję count_vowels() i wykonaj poniższe operacje:
-#
-# count_vowels('Python')
-#
-# count_vowels('Hello Pythonista!')
-#
-#
-#
-# Wyniki otrzymanych operacji wydrukuj do konsoli tak jak pokazano poniżej.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [OUT]:
 # 1
 # 5
+
 from string_operations_114 import count_vowels
 print(count_vowels('Python'))
 print(count_vowels('Hello Pythonista!'))
