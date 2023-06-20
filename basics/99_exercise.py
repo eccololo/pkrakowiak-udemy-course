d1d3ea2db62edbedf0183b698e346ab69ca28b90
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:19:35 +0200

    Change exercise desc in exercise 99.

--- basics/99_exercise.py
+++ basics/99_exercise.py
@@ -1,11 +1,3 @@
-# Napisz generator o nazwie fibonacci(), który będzie zwracać kolejne liczby z ciągu Fibonacciego. Generator powinien działać w nieskończoność, tzn. zwracać kolejne liczby aż do przekroczenia zakresu typu int.
-#
-#
-#
-# Przykłady użycia:
-#
-#
-#
 # [IN]: fib = fibonacci()
 # [IN]: next(fib)
 # [OUT]: 0
@@ -19,9 +11,6 @@
 # [OUT]: 3
 # [IN]: next(fib)
 # [OUT]: 5
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować generator. Nie trzeba go wywoływać. Zaimplementowane testy sprawdzają poprawność działania generatora.
 
 def fibonacci():
     nth1 = 0
