b3327076e884cb73911b50613b3861d426998de1
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:43:19 +0200

    Change exercise desc in exercise 121.

--- basics/121_exercise.py
+++ basics/121_exercise.py
@@ -1,32 +1,15 @@
 # Zdefiniuj klasę Car z czterema atrybutami klasy:
-#
 # brand
-#
 # model
-#
 # year
-#
 # color
-#
-#
-#
+
 # Ustaw wartości tych atrybutów odpowiednio na:
-#
 # 'Toyota'
-#
 # 'Camry'
-#
 # 2023
-#
 # 'White'
-#
-#
-#
-# Te atrybuty będą wspólne dla wszystkich instancji klasy i są dostępne za pomocą nazwy klasy.
-#
-#
-#
-# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli.
+
 
 class Car:
     brand = 'Toyota'
