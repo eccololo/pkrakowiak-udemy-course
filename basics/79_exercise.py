a2be48f45e953c3ae886cf1d2c932ecfbc704c77
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:58:54 +0200

    Change exercise desc in exercise 79.

--- basics/79_exercise.py
+++ basics/79_exercise.py
@@ -1,18 +1,6 @@
-# Podana jest poniższa lista:
-#
-#
-#
-# numbers = [5, 2, 8, 1, 9, 3]
-#
-#
-# Wykorzystując odpowiednie funkcje wbudowane wyznacz wartość największą, wartość najmniejszą i długość podanej listy. Wynik wydrukuj do konsoli tak jak pokazano poniżej.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [IN]: [5, 2, 8, 1, 9, 3]
+
+# [OUT]:
 # Maximum: 9
 # Minimum: 1
 # Length: 6
