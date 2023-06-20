3c54cde7fd5622a7cf7f1a875ea8af1f9b697ef2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:05:27 +0200

    Change exercise desc in exercise 86.

--- basics/86_exercise.py
+++ basics/86_exercise.py
@@ -1,15 +1,4 @@
-# Napisz funkcję print_odd_numbers(), która zwróci listę liczb nieparzystych większych od zera i mniejszych od 20.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-#
-#
-# Uwaga: Aby zaliczyć ćwiczenie wystarczy zdefiniować funkcję. Nie trzeba jej wywoływać. Zaimplementowane testy sprawdzają poprawność działania funkcji.
+# [OUT]: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 def print_odd_numbers():
     return [number for number in range(1, 20) if number % 2 != 0]
