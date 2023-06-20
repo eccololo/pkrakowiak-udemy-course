f52ceb092320ad918d510f1b27977965d460ea94
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 11:54:59 +0200

    Change exercise desc in exercise 76.

--- basics/76_exercise.py
+++ basics/76_exercise.py
@@ -1,59 +1,16 @@
-# Podany
-# jest
-# poniższy
-# słownik
-# zawierający
-# dane
-# giełdowe:
-#
-# stock_data = {
+# [IN]: {
 #     'AAPL': 145.9,
 #     'GOOG': 2680.7,
 #     'TSLA': 712.6,
 #     'AMZN': 3379.1
 # }
-#
-# Zapisz
-# te
-# dane
-# do
-# pliku
-# tekstowego
-# stock_prices.txt.Każdą
-# spółkę
-# zapisz
-# jako
-# wiersz, który
-# składa
-# się
-# z
-# nazwy
-# spółki
-# i
-# ceny
-# akcji, oddzielonych
-# średnikiem.Każdy
-# wiersz
-# zakończ
-# znakiem
-# nowej
-# linii \n.
-#
-# Zawartość
-# pliku
-# stock_prices.txt
-# po
-# zapisaniu
-# danych:
-#
-# AAPL;
-# 145.9
-# GOOG;
-# 2680.7
-# TSLA;
-# 712.6
-# AMZN;
-# 3379.1
+
+# [OUT]: 76_stock_prices.txt file:
+# AAPL;145.9
+# GOOG;2680.7
+# TSLA;712.6
+# AMZN;3379.1
+
 
 stock_data = {
     'AAPL': 145.9,
