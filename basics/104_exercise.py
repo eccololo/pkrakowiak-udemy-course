473b3b786c7fa5fa8cbe4edbb93e014a17bc8d88
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:23:21 +0200

    Change exercise desc in exercise 104.

--- basics/104_exercise.py
+++ basics/104_exercise.py
@@ -1,25 +1,12 @@
-# Załóżmy, że masz listę słowników, z których każdy reprezentuje produkt w twoim sklepie e-commerce. Każdy produkt posiada nazwę, cenę oraz ilość dostępną na magazynie:
-#
-#
-#
-# products = [
+# [IN]: [
 #     {'name': 'T-shirt', 'price': 20, 'quantity': 10},
 #     {'name': 'Jeans', 'price': 50, 'quantity': 0},
 #     {'name': 'Sneakers', 'price': 80, 'quantity': 5},
 #     {'name': 'Hat', 'price': 15, 'quantity': 3},
 #     {'name': 'Backpack', 'price': 30, 'quantity': 7}
 # ]
-#
-#
-# Chcesz utworzyć nową listę o nazwie in_stock zawierającą tylko nazwy produktów, które są aktualnie w magazynie (tj. quantity > 0). Do rozwiązania tego problemu wykorzystaj list comprehension. W odpowiedzi wydrukuj do konsoli listę in_stock.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
-# ['T-shirt', 'Sneakers', 'Hat', 'Backpack']
+
+# [OUT]: ['T-shirt', 'Sneakers', 'Hat', 'Backpack']
 
 products = [
     {'name': 'T-shirt', 'price': 20, 'quantity': 10},
