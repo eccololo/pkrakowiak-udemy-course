a3a738a65a0c7cb6dd56df7620bf4c09c7636859
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:36:33 +0200

    Change exercise desc in exercise 113.

--- basics/113_exercise.py
+++ basics/113_exercise.py
@@ -1,25 +1,4 @@
-# Podany jest moduł o nazwie math_functions.py, który zawiera funkcje związane z prostymi działaniami matematycznymi. Funkcji tych chcesz użyć w innym pliku o nazwie exercise.py.
-#
-#
-#
-# Zaimportuj odpowiednio moduł math_functions i wykonaj poniższe operacje:
-#
-# square(10)
-#
-# cube(3)
-#
-# add(10, 5)
-#
-#
-#
-# Wyniki otrzymanych operacji wydrukuj do konsoli tak jak pokazano poniżej.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [OUT]:
 # 100
 # 27
 # 15
