46785e9f8a438b0d54b5bafe7fcebe236b75d06b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:01:56 +0200

    Change exercise desc in exercise 82.

--- basics/82_exercise.py
+++ basics/82_exercise.py
@@ -1,54 +1,11 @@
-# Podane
-# są
-# poniższe
-# zmienne:
-#
+# [IN]:
 # x = 5
 # y = 3
-#
-# expression1 = '2 * 3 + 5'
-# expression2 = 'x + y'
-# expression3 = 'max([5, 2, 8, 1, 9, 3])'
-#
-# Wykorzystując
-# odpowiednią
-# funkcję
-# wbudowaną, wykonaj
-# wyrażenia
-# języka
-# Python
-# podane
-# jako
-# ciąg
-# znaków - zmienne
-# expression1, expression2
-# i
-# expression3.Wynik
-# wydrukuj
-# do
-# konsoli
-# tak
-# jak
-# pokazano
-# poniżej.
-#
-# Oczekiwany
-# wynik:
-#
-# The
-# result
-# of
-# the
-# expression1 is: 11
-# The
-# result
-# of
-# the
-# expression2 is: 8
-# The
-# maximum
-# value in the
-# list is: 9
+
+# [OUT]:
+# The result of the expression1 is: 11
+# The result of the expression1 is: 8
+# The maximum value in the list is: 9
 
 x = 5
 y = 3
