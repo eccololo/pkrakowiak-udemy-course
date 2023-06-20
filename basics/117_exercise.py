0741cd10c3e17cb65224f1230a7eb1fe53f3b12e
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-20 12:40:05 +0200

    Change exercise desc in exercise 117.

--- basics/117_exercise.py
+++ basics/117_exercise.py
@@ -1,23 +1,8 @@
-# Załóżmy, że pracujesz nad projektem dotyczącym energii odnawialnej i musisz wykonać obliczenia związane z energią słoneczną. Utworzyłeś już moduł o nazwie solar.py, który zawiera funkcje do wykonywania obliczeń, takich jak natężenie promieniowania słonecznego - solar_irradiance(), wydajność modułu fotowoltaicznego - pv_module_efficiency() i moc wyjściowa - power_output().
-#
-#
-#
-# Oblicz natężenie promieniowania słonecznego w W/m^2, biorąc pod uwagę powierzchnię i moc modułu fotowoltaicznego o podanych parametrach:
-#
-# area = 1.5 m^2
-#
-# power = 300 W
-#
-#
-#
-# Wynik wydrukuj do konsoli.
-#
-#
-#
-# Oczekiwany wynik:
-#
-#
-#
+# [IN]:
+# area = 1.5
+# power = 300
+
+# [OUT]:
 # 300.0
 
 from solar_117 import *
