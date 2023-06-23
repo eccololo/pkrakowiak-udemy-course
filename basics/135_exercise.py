4d92035d5e0358e20b6045a63d5ac23243faee11
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-23 18:00:29 +0200

    Change exercise desc in exercise 135.

--- basics/135_exercise.py
+++ basics/135_exercise.py
@@ -1,19 +1,5 @@
-# Załóżmy, że pracujesz nad systemem do obsługi produkcji samolotów pasażerskich. Podana jest poniższa klasa bazowa Component, która ma zaimplementowaną metodę __init__() do ustawiania atrybutów name i weight.
-#
-#
-#
-# class Component:
-#     def __init__(self, name, weight):
-#         self.name = name
-#         self.weight = weight
-#
-#
-# Zaimplementuj klasę o nazwie Wing, która będzie dziedziczyć po klasie Component dodając dodatkowy atrybut span.
-#
-#
-#
-# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli. Testy jednostkowe sprawdzają poprawność implementacji rozwiązania.
-#
+# Zaimplementuj klase rodzica Component oraz klase dzicko Wing ktora dziedziczy po Component.
+# W klasie Component daj name i weight a w klasie dziecka dodaj span.
 
 class Component:
     def __init__(self, name, weight):
