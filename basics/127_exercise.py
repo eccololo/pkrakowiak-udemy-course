bbad418892cb54fc30a8e5b99411f4b23e0b48c8
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-23 17:51:03 +0200

    Change exercise desc in exercise 127.

--- basics/127_exercise.py
+++ basics/127_exercise.py
@@ -1,8 +1,4 @@
-# Zaimplementuj klasę Weather, która ma dwa atrybuty instancji: temperature i humidity, które są inicjowane w metodzie konstruktora __init__(). W klasie Weather zaimplementuj również metodę statyczną celsius_to_fahrenheit(), która pobiera temperaturę w stopniach Celsjusza i zwraca równoważną temperaturę w stopniach Fahrenheita. Pamiętaj, że metoda ta ma być metodą statyczną.
-#
-#
-#
-# Uwaga! W celu zaliczenia ćwiczenia należy tylko zaimplementować klasę. Nie trzeba niczego drukować do konsoli. Testy jednostkowe sprawdzają poprawność implementacji rozwiązania.
+# Zaimplementuj klase Weather ktora zamieni stopnie Celcjusza na Fahrenheita.
 
 class Weather:
 
