9acf30e59883f862d4bcc6e93d5a97fbe13c74f6
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-26 14:31:09 +0200

    Exercise 144 done.

---
+++ basics/144_exercise.py
@@ -0,0 +1,42 @@
+# [IN]: z1 = ComplexNumber(2, 3)
+# [IN]: z2 = ComplexNumber(-1, 2)
+# [IN]: z1 + z2
+# [OUT]: ComplexNumber(1, 5)
+#
+#
+# [IN]: z1 = ComplexNumber(2, 3)
+# [IN]: z2 = ComplexNumber(-1, 2)
+# [IN]: z1 - z2
+# [OUT]: ComplexNumber(3, 1)
+
+
+class ComplexNumber:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __str__(self):
+        if self.imag < 0:
+            return f'{self.real} - {-self.imag}i'
+        else:
+            return f'{self.real} + {self.imag}i'
+
+    # Enter your solution here
+
+    def __add__(self, other):
+        return f'ComplexNumber({self.real + other.real}, {self.imag + other.imag})'
+
+    def __sub__(self, other):
+        return f'ComplexNumber({self.real - other.real}, {self.imag - other.imag})'
+
+
+z1 = ComplexNumber(2, 3)
+z2 = ComplexNumber(-1, 2)
+print(z1 + z2)
+# [OUT]: ComplexNumber(1, 5)
+#
+#
+z1 = ComplexNumber(2, 3)
+z2 = ComplexNumber(-1, 2)
+print(z1 - z2)
+# [OUT]: ComplexNumber(3, 1)
