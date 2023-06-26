0c9e303acc27caf647f2bd3ebae59e8b0d0e62b4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-26 15:01:24 +0200

    Exercise 147 done.

---
+++ basics/147_exercise.py
@@ -0,0 +1,18 @@
+def rectangle_area(width, height):
+    """
+    Calculates the area of a rectangle with given width and height.
+
+    :param width: The width of the rectangle.
+    :param height: The height of the rectangle.
+    :return: The area of the rectangle.
+    """
+    return width * height
+
+
+# Enter your solution here
+assert rectangle_area(4, 10) == 40
+assert rectangle_area(5, 6) == 30
+
+
+
+
