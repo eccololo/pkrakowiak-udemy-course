b2e0c4f326bacc0ef3fb181314b1ae16db93b782
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 15:10:51 +0200

    Exercise 33.1 in course 2 done.

---
+++ course-2-exercises/33.1_exercise.py
@@ -0,0 +1,11 @@
+num_of_users = 9045
+num_of_posts = 54822
+num_of_likes = 251561
+
+# Enter your soluion here
+avg_posts_per_user = num_of_posts / num_of_users
+avg_likes_per_post = num_of_likes / num_of_posts
+
+print(f"Average number of posts per user: {avg_posts_per_user:.2f}")
+print(f"Average number of likes per post: {avg_likes_per_post:.2f}")
+
