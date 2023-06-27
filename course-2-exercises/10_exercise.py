c8acf5335a3dfe48f4e9cc96a7408da804793624
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:05:49 +0200

    Exercise 10 done.

---
+++ course-2-exercises/10_exercise.py
@@ -0,0 +1,11 @@
+store_name = 'Shopshoe'
+item_name = 'Running shoes'
+item_price = 100.00
+item_discount = 0.30
+
+item_price_discounted = item_price * (1 - item_discount)
+
+print(f'Welcome to {store_name}!')
+print('-' * 50)
+print(f"Today's special is the {item_name}, which normally costs ${item_price}.")
+print(f"But for a limited time, you can get it for ${item_price_discounted} ({int(item_discount * 100)}% off)!")
