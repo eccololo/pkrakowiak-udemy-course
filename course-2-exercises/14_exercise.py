fd7a311be8fe3a6cd5f4eec99dcc416addd2b943
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:24:03 +0200

    Exercise 14 done.

---
+++ course-2-exercises/14_exercise.py
@@ -0,0 +1,8 @@
+experiment_name = 'The Effects of Temperature on Enzyme Activity'
+treatment_group = 'Group A'
+enzyme_activity = 12.3456
+
+# Enter your solution here
+
+print('Welcome to The Effects of Temperature on Enzyme Activity!')
+print(f'The Group A enzyme activity level is {enzyme_activity:.2f} units.')
