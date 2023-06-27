be1041c515f4712b688cae681b3022dee0da0c3c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:58:23 +0200

    Exercise 31 in course 2 done.

---
+++ course-2-exercises/31_exercise.py
@@ -0,0 +1,10 @@
+num_of_cars = 100
+num_of_parts = 1000
+cost_per_part = 10
+
+# Enter your solution here
+total_cost = num_of_cars * num_of_parts * cost_per_part
+avg_cost_per_car = total_cost / num_of_cars
+
+print(f"Total cost of manufacturing all the cars: {total_cost}")
+print(f"Average cost per car: {avg_cost_per_car}")
