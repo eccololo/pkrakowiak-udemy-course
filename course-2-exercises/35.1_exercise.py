247e4206dd7035b44711abe10e37cdabd7497015
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 15:24:45 +0200

    Exercise 35.1 in course 2 done.

---
+++ course-2-exercises/35.1_exercise.py
@@ -0,0 +1,7 @@
+total_num_posts = 47
+posts_per_page = 10
+
+# Enter your solution here
+total_num_pages = total_num_posts // posts_per_page + 1
+
+print(f"The total number of pages required to display all the posts is: {total_num_pages}")
