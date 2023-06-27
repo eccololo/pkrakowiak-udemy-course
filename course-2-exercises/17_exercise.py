4a8f5cc15347a6503a22a10ab23d31996cd5673b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:51:18 +0200

    Exercise 17 in course 2 done.

---
+++ course-2-exercises/17_exercise.py
@@ -0,0 +1,21 @@
+item1 = 'Apple'
+quantity1 = 10
+price1 = 1.50
+
+item2 = 'Banana'
+quantity2 = 5
+price2 = 0.75
+
+item3 = 'Cherry'
+quantity3 = 20
+price3 = 0.25
+
+# Enter your solution here
+
+print("+---------+-------------+-----------+")
+print("| Item    | Quantity    | Price     |")
+print("+---------+-------------+-----------+")
+print(f"| {item1:<8}| {quantity1:<12}| ${price1:<9.2f}|")
+print(f"| {item2:<8}| {quantity2:<12}| ${price2:<9.2f}|")
+print(f"| {item3:<8}| {quantity3:<12}| ${price3:<9.2f}|")
+print("+---------+-------------+-----------+")
