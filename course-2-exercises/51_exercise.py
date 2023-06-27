22c22c456d9ab23a58adc9c25ceced517d139aa5
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 16:05:24 +0200

    Exercise 51 in course 2 done.

---
+++ course-2-exercises/51_exercise.py
@@ -0,0 +1,13 @@
+followers = ['john', 'jane', 'jacob', 'jessica', 'jenny']
+profile_picture = 'https://example.com/profile.jpg'
+post_ids = [1, 2, 3, 4, 5]
+bio = 'Travel enthusiast | Photographer | Nature lover'
+timestamp = 1649499360
+
+# Enter your solution here
+
+print(type(followers))
+print(type(profile_picture))
+print(type(post_ids))
+print(type(bio))
+print(type(timestamp))
