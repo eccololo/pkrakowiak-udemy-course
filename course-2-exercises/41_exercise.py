2e49ca859313c83ef5c9642cd9b526ec114b1696
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 15:35:30 +0200

    Exercise 41 in course 2 done.

--- course-2-exercises/41_exercise.py
+++ course-2-exercises/41_exercise.py
@@ -1,5 +1,7 @@
-normal_list = list(range(1, 10))
+normal_list = list(range(0, 10))
 first_3 = normal_list[:3]
 last_3 = normal_list[-3:]
-print(first_3)
-print(last_3)
+
+print(f"Original list: {normal_list}")
+print(f"First three elements: {first_3}")
+print(f"Last three elements: {last_3}")
