5af6be42ec5f291c9e4b218794fa8820d374b8cb
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 15:47:37 +0200

    Exercise 44.1 in course 2 done.

---
+++ course-2-exercises/44.1_exercise.py
@@ -0,0 +1,10 @@
+dimensions = '10x5x2'
+
+# Enter your solution here
+length = dimensions[:2]
+width = dimensions[3:4]
+height = dimensions[-1:]
+
+print(f"length = {length}")
+print(f"width = {width}")
+print(f"height = {height}")
