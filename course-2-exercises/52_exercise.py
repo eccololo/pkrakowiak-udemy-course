9dea407b1b2dd3b54194f6280aebac6655fbc2b1
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 16:08:19 +0200

    Exercise 52 in course 2 done.

---
+++ course-2-exercises/52_exercise.py
@@ -0,0 +1,12 @@
+followers = ['john', 'jane', 'jacob', 'jessica', 'jenny']
+profile_picture = 'https://example.com/profile.jpg'
+post_ids = [1, 2, 3, 4, 5]
+bio = 'Travel enthusiast | Photographer | Nature lover'
+timestamp = 1649499360
+
+# Enter your solution here
+print(isinstance(followers, tuple))
+print(isinstance(profile_picture, str))
+print(isinstance(post_ids, list))
+print(isinstance(bio, set))
+print(isinstance(timestamp, float))
