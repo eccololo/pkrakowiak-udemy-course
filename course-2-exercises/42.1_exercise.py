d3a37cd7a707f1c9a87f86ee60610a1584ba3400
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 15:39:01 +0200

    Exercise 42.1 in course 2 done.

---
+++ course-2-exercises/42.1_exercise.py
@@ -0,0 +1,9 @@
+personnel = [
+    'John Smith, Private First Class',
+    'Jane Doe, Captain',
+    'Bob Johnson, Sergeant'
+]
+
+# Enter your solution here
+sergant_name = personnel[-1:][0].split(",")[0]
+print(sergant_name)
