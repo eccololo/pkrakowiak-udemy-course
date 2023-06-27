8a7ee34222d2867cb13830ff5fb194438a23bee7
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:06:47 +0200

    Exercise 10 done.

--- course-2-exercises/10_exercise.py
+++ course-2-exercises/10_exercise.py
@@ -7,5 +7,5 @@ item_price_discounted = item_price * (1 - item_discount)
 
 print(f'Welcome to {store_name}!')
 print('-' * 50)
-print(f"Today's special is the {item_name}, which normally costs ${item_price}.")
-print(f"But for a limited time, you can get it for ${item_price_discounted} ({int(item_discount * 100)}% off)!")
+print(f"Today's special is the {item_name}, which normally costs ${item_price:.2f}.")
+print(f"But for a limited time, you can get it for ${item_price_discounted:.2f} ({int(item_discount * 100)}% off)!")
