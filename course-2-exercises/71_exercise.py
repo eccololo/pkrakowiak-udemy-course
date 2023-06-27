a8bb4dcfa7830a1bb04d399a3e0f58c7731be880
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 16:34:18 +0200

    Exercise 71 in course 2 done.

---
+++ course-2-exercises/71_exercise.py
@@ -0,0 +1,12 @@
+gate_number = 'B12'
+flight_number = '123'
+seat_assignment = '24A'
+
+# Enter your solution here
+gate_number = "0" + gate_number
+flight_number = "000" + flight_number
+seat_assignment = "0" + seat_assignment
+
+print(gate_number)
+print(flight_number)
+print(seat_assignment)
