75f3bfcd90ee3e1f2b842fd19cd63fb916d279da
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-27 14:11:03 +0200

    Exercise 11 done.

---
+++ course-2-exercises/11_exercise.py
@@ -0,0 +1,21 @@
+brand = 'Ford'
+model = 'Mustang'
+year = 2021
+color = 'Red'
+mileage = 5000
+
+print(f"""CAR DETAILS
+Make: {brand}
+Model: {model}
+Year: {year}
+Color: {color}
+Mileage: {mileage} miles""")
+
+
+
+
+
+
+
+
+
