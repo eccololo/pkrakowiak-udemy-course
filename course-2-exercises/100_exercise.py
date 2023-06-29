2e99387439c107e3d9d95a0df08c1409e51b6927
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 11:00:36 +0200

    Exercise 100 in course 2 done.

---
+++ course-2-exercises/100_exercise.py
@@ -0,0 +1,10 @@
+order_queue = []
+order_queue.append('Pen')
+order_queue.append(('Book', 'Mouse'))
+order_queue.append(('Notebook', 'Pencil'))
+
+print(order_queue[0])
+order_queue.pop(0)
+print(order_queue[0])
+order_queue.pop(0)
+print(order_queue[0])
