f25177f2220aab0149bc05a96dbe3aa63d8c71e2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 11:16:20 +0200

    Exercise 102 in course 2 done.

---
+++ course-2-exercises/102_exercise.py
@@ -0,0 +1,57 @@
+menu = [
+    [
+        'Spaghetti Bolognese',
+        12.99,
+        [
+            'spaghetti',
+            'tomato sauce',
+            'ground beef',
+            'onion',
+        ],
+    ],
+    [
+        'Caesar Salad',
+        8.99,
+        [
+            'romaine lettuce',
+            'croutons',
+            'parmesan cheese',
+            'Caesar dressing',
+        ],
+    ],
+    [
+        'Margherita Pizza',
+        14.99,
+        [
+            'pizza dough',
+            'tomato sauce',
+            'mozzarella cheese',
+            'basil',
+        ],
+    ],
+    [
+        'Cheeseburger',
+        10.99,
+        [
+            'beef patty',
+            'cheddar cheese',
+            'lettuce',
+            'tomato',
+            'bun',
+        ],
+    ],
+]
+
+# Enter your solution here
+
+menu.append([
+    'Chicken Alfredo',
+    15.99,
+    [
+        'fettuccine',
+        'alfredo sauce',
+        'chicken',
+    ],
+])
+
+print(menu[-1])
