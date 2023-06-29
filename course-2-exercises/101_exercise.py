5b7683a28e55a7bf59445a30fa01149ab8ea8ab5
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 11:03:17 +0200

    Exercise 101 in course 2 done.

---
+++ course-2-exercises/101_exercise.py
@@ -0,0 +1,46 @@
+menu = [
+    [
+        'Spaghetti Bolognese',
+        12.99,
+        [
+            'spaghetti',
+            'tomato sauce',
+            'ground beef',
+            'onion',
+        ],
+    ],
+    [
+        'Caesar Salad',
+        8.99,
+        [
+            'romaine lettuce',
+            'croutons',
+            'parmesan cheese',
+            'Caesar dressing',
+        ],
+    ],
+    [
+        'Margherita Pizza',
+        14.99,
+        [
+            'pizza dough',
+            'tomato sauce',
+            'mozzarella cheese',
+            'basil',
+        ],
+    ],
+    [
+        'Cheeseburger',
+        10.99,
+        [
+            'beef patty',
+            'cheddar cheese',
+            'lettuce',
+            'tomato',
+            'bun',
+        ],
+    ],
+]
+
+# Enter your solution here
+print(sorted(menu[2][2]))
