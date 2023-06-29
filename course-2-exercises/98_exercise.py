7c10fe33c5ce49771d1625744f858629ce2d4bfd
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 10:43:07 +0200

    Exercise 98 in course 2 done.

---
+++ course-2-exercises/98_exercise.py
@@ -0,0 +1,13 @@
+players = ['LeBron', 'Kobe', 'Jordan', 'Durant']
+scores = [27, 18, 22, 15]
+
+# Enter your solution here
+
+new_players = ['LeBron', 'Kobe']
+new_scores = [27, 18]
+
+united_players = players + new_players
+united_scores = scores + new_scores
+
+print(united_players)
+print(united_scores)
