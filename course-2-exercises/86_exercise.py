be306bbe5d301fd01cc0912e60e9d09b017eca51
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 10:25:11 +0200

    Exercise 86 in course 2 done.

---
+++ course-2-exercises/86_exercise.py
@@ -0,0 +1,10 @@
+objectives1 = ('Secure the perimeter', 'Neutralize enemy targets')
+objectives2 = ('Provide medical assistance', 'Evacuate civilians')
+
+coordinates1 = ((35.6895, 139.6917), (34.0537, -118.2424))
+coordinates2 = ((40.7128, -74.0060), (51.5074, -0.1278))
+
+# Enter your solution here
+
+print(f"First mission: {objectives1 + coordinates1}")
+print(f"Second mission: {objectives2 + coordinates2}")
