eb71dd82aa9a750d7f0322e5e375318a11ba97de
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 11:34:19 +0200

    Exercise 120 in course 2 done.

---
+++ course-2-exercises/120_exercise.py
@@ -0,0 +1,11 @@
+profile = {
+    'name': 'John Doe',
+    'age': 25,
+    'location': 'New York',
+    'interests': ['photography', 'music', 'hiking'],
+}
+
+# Enter your solution here
+profile["location"] = 'Los Angeles'
+profile["interests"].append("travel")
+print(profile)
