fe4a23449d21441c21ddabf034a7e98d15b8b20c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 11:23:40 +0200

    Exercise 104.1 in course 2 done.

---
+++ course-2-exercises/104.1_exercise.py
@@ -0,0 +1,40 @@
+flights = [
+    [
+        'United Airlines',
+        'UA123',
+        'New York',
+        'Los Angeles',
+        '10:00 AM',
+    ],
+    [
+        'Delta Airlines',
+        'DL456',
+        'Chicago',
+        'Houston',
+        '11:30 AM',
+    ],
+    [
+        'American Airlines',
+        'AA789',
+        'Dallas',
+        'San Francisco',
+        '08:15 AM',
+    ],
+    [
+        'Southwest Airlines',
+        'WN012',
+        'Los Angeles',
+        'Denver',
+        '09:45 AM',
+    ],
+]
+
+# Enter your solution here
+departures = [
+    flights[0][-1],
+    flights[1][-1],
+    flights[2][-1],
+    flights[3][-1]
+]
+
+print(departures)
