7b6d48d51aefcaa048859115acfdd91ac2bf314a
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 10:40:50 +0200

    Exercise 97 in course 2 done.

---
+++ course-2-exercises/97_exercise.py
@@ -0,0 +1,11 @@
+players = ['LeBron', 'Kobe', 'Jordan', 'Durant']
+scores = [27, 18, 22, 15]
+
+# Enter your solution here\
+
+players.remove("Kobe")
+popped_score = scores.pop(1)
+
+print(players)
+print(scores)
+print(popped_score)
