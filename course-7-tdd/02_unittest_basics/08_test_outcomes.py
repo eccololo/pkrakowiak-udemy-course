1a81b878e268120b907cdb8c0e7ead07cead1248
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-08 16:04:28 +0200

    TDD lesson - script 8 - learning about unittest basics - done.

---
+++ course-7-tdd/02_unittest_basics/08_test_outcomes.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+def area(w, h):
+    if not (isinstance(w, (int, float)) and isinstance(h, (int, float))):
+        raise TypeError("Width and height must be value of type int or float.")
+
+    if not (w > 0 and h > 0):
+        raise ValueError("Width and height must be value greater than 0.")
+
+    return w * h
+
+
+class TestArea(unittest.TestCase):
+
+    def test_area_1(self):
+        self.assertEqual(area(4, 5), 20, "Expected area is not equal to calculated.")
+
+    def test_area_2(self):
+        self.assertEqual(area(4, 5), 21, "Expected area is not equal to calculated.")
+
+    def test_area_3(self):
+        raise AssertionError("Error message 3.")
+
+    def test_area_4(self):
+        raise TypeError("Error message 4.")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
