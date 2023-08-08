419e21eca8ac4713046b352c3a4b5b9046f9d822
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-08 16:11:19 +0200

    TDD lesson - bonus - learning about __name__ variable - done.

---
+++ course-7-tdd/02_unittest_basics/bonus/first.py
@@ -0,0 +1,5 @@
+print(__name__)
+print("Info: Script first.")
+
+if __name__ == "__main__":
+    print("Script first")
---
+++ course-7-tdd/02_unittest_basics/bonus/second.py
@@ -0,0 +1,6 @@
+print(__name__)
+print("Info: Script second.")
+
+import first
+
+print("Module name:", first.__name__)
