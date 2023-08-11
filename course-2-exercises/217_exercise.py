e9e30e8923f7a24c43ff1b932f39aa3f8b36f3af
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-11 15:45:42 +0200

    Course 2 - Exercise 217 - done

---
+++ course-2-exercises/217_exercise.py
@@ -0,0 +1,12 @@
+numbers = [3, -5, -2, 8, 1, 9]
+
+# Enter your solution here
+max_number = max(numbers)
+min_number = min(numbers)
+sum_numbers = sum(numbers)
+abs_min = abs(min_number)
+
+print(max_number)
+print(min_number)
+print(sum_numbers)
+print(abs_min)
