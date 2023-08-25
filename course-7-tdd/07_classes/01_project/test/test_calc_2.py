51005471ada24932353de8b9a1ccb60342828e1c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-25 12:28:28 +0200

    Course 4 - TDD with Classes - Exercise 2 - Done

--- course-7-tdd/07_classes/01_project/test/test_calc_2.py
+++ course-7-tdd/07_classes/01_project/test/test_calc_2.py
@@ -2,16 +2,25 @@ import unittest
 from calculator.calc_math import SimpleMathCalculator
 
 
+def setUpModule():
+    print("Setting up ...")
+    global calc
+    calc = SimpleMathCalculator()
+
+
+def tearDownModule():
+    print("Tearing down ...")
+    global calc
+    del calc
+
+
 class TestSimpleMathCalculator(unittest.TestCase):
 
     def test_add(self):
-        calc = SimpleMathCalculator()
         self.assertEqual(calc.add(3, 4), 7)
 
     def test_subtract(self):
-        calc = SimpleMathCalculator()
         self.assertEqual(calc.subtract(3, 4), -1)
 
     def test_multiply(self):
-        calc = SimpleMathCalculator()
         self.assertEqual(calc.multiply(3, 4), 12)
