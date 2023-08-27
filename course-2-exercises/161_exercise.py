bf6316dfaf13f52038ac82c6488c3a812a45e911
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-27 14:23:18 +0200

    Course 2 - Exercise 161 - Done

---
+++ course-2-exercises/161_exercise.py
@@ -0,0 +1,20 @@
+import math
+
+energy_output = [
+    0.5,
+    0.6,
+    0.7,
+    0.8,
+    0.9,
+    1.0,
+    0.9,
+    0.8,
+    0.7,
+    0.6,
+    0.5,
+    0.4,
+]
+
+# Enter your solution here
+
+print(f"Total energy generated: {math.floor(sum(energy_output)):.2f}")
