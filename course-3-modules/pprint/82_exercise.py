b5a69485a29bbf5edaf0549053c8288c4007f4dd
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-09-04 09:26:14 +0200

    Course 3 - Exercise 82 - Done

---
+++ course-3-modules/pprint/82_exercise.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+
+data_dict = {
+    "users": [
+        {
+            "userId": 1,
+            "firstName": "Krish",
+            "lastName": "Lee",
+            "emailAddress": "krish.lee@example.com"
+        },
+        {
+            "userId": 2,
+            "firstName": "racks",
+            "lastName": "jacson",
+            "emailAddress": "racks.jacson@example.com"
+        },
+        {
+            "userId": 3,
+            "firstName": "denial",
+            "lastName": "roast",
+            "emailAddress": "denial.roast@example.com"
+        }
+    ]
+}
+
+pprint(data_dict)
