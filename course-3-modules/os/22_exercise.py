65cc37b79fe13c91072e9c4a19ed8f3883730024
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-02-20 14:37:09 +0100

    Course 3 - Exercise 22 - Done

---
+++ course-3-modules/os/22_exercise.py
@@ -0,0 +1,43 @@
+import os
+import random
+
+
+random.seed(30)
+images = [
+    f"{str(i).zfill(3)}_image.{random.choice(['png', 'jpg'])}"
+    for i in range(1, 20)
+]
+
+base_dir = 'images'
+png_dir = os.path.join(base_dir, 'images_png')
+jpg_dir = os.path.join(base_dir, 'images_jpg')
+
+if not os.path.exists(base_dir):
+    os.mkdir(base_dir)
+
+if not os.path.exists(png_dir):
+    os.mkdir(png_dir)
+
+if not os.path.exists(jpg_dir):
+    os.mkdir(jpg_dir)
+
+for image in images:
+    if image.endswith('.png'):
+        open(os.path.join(png_dir, image), 'w').close()
+    elif image.endswith('.jpg'):
+        open(os.path.join(jpg_dir, image), 'w').close()
+
+png_files = []
+jpg_files = []
+for root, dirs, files in os.walk(base_dir):
+    if len(files) > 0:
+        for file in files:
+            if file.endswith("png"):
+                png_files.append(file)
+            else:
+                jpg_files.append(file)
+
+else:
+    output = png_files + jpg_files
+    for file in output:
+        print(file)
