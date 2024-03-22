6c3c2d21d02e5ae765361546d09beee4c46f5d9d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-22 19:44:33 +0100

    Exercise 61 - done.

---
+++ course-3-modules/collections/61_exercise.py
@@ -0,0 +1,9 @@
+from collections import ChainMap
+
+default_settings = {"mode": "eco", "power_level": 7}
+user_settings = {"mode": "sport", "power_level": 10}
+
+settings = ChainMap(user_settings, default_settings)
+
+print(settings['mode'])
+
